47e8b6c07fe79a7a63f6e459a96863dc5ef18012
prokoin <126255665+prokoin@users.noreply.github.com>
2024-04-25 17:41:58 +0700

    test

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -5,23 +5,21 @@ export const config = {
 };
 
 export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-  const hostname = req.headers.get("host")!;
-  const path = url.pathname;
-  let subdomain = hostname.split(".")[0];
-  subdomain = subdomain.replace("localhost:3000", "");
-  subdomain = subdomain.replace("showdev.tech", "");
-  if (subdomain === "www" || subdomain === "") {
-    return NextResponse.next();
-  }
-  if (subdomain === "app" || subdomain === "pemodal") {
-    return NextResponse.rewrite(new URL(`/pemodal/${path === "/" ? "" : path}`, req.url));
-  } else if (subdomain === "app" || subdomain === "penerbit") {
-    return NextResponse.rewrite(new URL(`/penerbit/${path === "/" ? "" : path}`, req.url));
-  }
-  return NextResponse.redirect(hostname);
-
+  // const url = req.nextUrl;
+  // const hostname = req.headers.get("host")!;
+  // const path = url.pathname;
+  // let subdomain = hostname.split(".")[0];
+  // subdomain = subdomain.replace("localhost:3000", "");
+  // subdomain = subdomain.replace("showdev.tech", "");
+  // if (subdomain === "www" || subdomain === "") {
+  //   return NextResponse.next();
+  // }
+  // if (subdomain === "app" || subdomain === "pemodal") {
+  //   return NextResponse.rewrite(new URL(`/pemodal/${path === "/" ? "" : path}`, req.url));
+  // } else if (subdomain === "app" || subdomain === "penerbit") {
+  //   return NextResponse.rewrite(new URL(`/penerbit/${path === "/" ? "" : path}`, req.url));
+  // }
+  // return NextResponse.redirect(hostname);
   // ==========
-
-  // return NextResponse.next();
+  return NextResponse.next();
 }
