8acea8c406bed212e9e0807d5e449a95f07ca5eb
prokoin <126255665+prokoin@users.noreply.github.com>
2024-04-25 13:09:14 +0700

    testt

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/pemodal/:path*",
-      },
-    ];
+    // return [
+    //   {
+    //     source: "/:path*",
+    //     destination: "/pemodal/:path*",
+    //   },
+    // ];
   },
 };
 
--- src/app/pemodal/page.tsx
+++ src/app/pemodal/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function HomePemodal() {
   return (
     <>
-      <h1>Pemodal Home</h1>
+      <h1>Pemodal Homes</h1>
     </>
   );
 }
