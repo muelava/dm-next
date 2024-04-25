65fb8d400a276af514385051d30e8e6d00cb29dd
prokoin <126255665+prokoin@users.noreply.github.com>
2024-04-25 13:43:56 +0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/pemodal/:path*", // Tentukan source untuk subdomain pemodal.showdev.tech
-        destination: "/src/app/pemodal/:path*", // Arahkan permintaan dari subdomain pemodal.showdev.tech ke folder src/app/pemodal
-      },
-      {
-        source: "/:path*", // Tentukan source untuk domain showdev.tech
-        destination: "/src/app/:path*", // Arahkan semua permintaan dari domain showdev.tech ke folder src/app
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/pemodal/:path*", // Tentukan source untuk subdomain pemodal.showdev.tech
+  //       destination: "/src/app/pemodal/:path*", // Arahkan permintaan dari subdomain pemodal.showdev.tech ke folder src/app/pemodal
+  //     },
+  //     {
+  //       source: "/:path*", // Tentukan source untuk domain showdev.tech
+  //       destination: "/src/app/:path*", // Arahkan semua permintaan dari domain showdev.tech ke folder src/app
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
